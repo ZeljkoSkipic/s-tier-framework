4560f6151cbf1623d08b98ba80f26f17f30b977a
zeljkoskipic <zeljko.skipic@gmail.com>
2023-08-31 21:11:30 +0200

    Initial
    Single Post
    Blog
    Functions
    Header
    Footer

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package s-tier
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="footer_inner container">
+			<div class="col copy">
+				© 2023 S-Tier Dev Digital Agency
+			</div>
+		</div>
+	</footer>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
