2f9147f062018a2f11d4fb52b57d633ba3ebff72
zeljkoskipic <zeljko.skipic@gmail.com>
2024-08-01 23:54:06 +0200

    Walker Fix

--- includes/custom-menu-walker.php
+++ includes/custom-menu-walker.php
@@ -3,11 +3,11 @@ class CustomMenuWalker extends Walker_Nav_Menu
 {
     function start_el(&$output, $item, $depth = 0, $args = [], $id = 0)
     {
-        $output .= "<li class='" .  implode(" ", $item->classes) . "'>";
+        $output .= "<li class='" .  implode(" ", (array) $item->classes) . "'>";
         $output .= '<a href="' . $item->url . '">';
         $output .= $item->title;
         $output .= '</a>';
-        if (in_array('menu-item-has-children', $item->classes)) {
+        if (in_array('menu-item-has-children', (array) $item->classes)) {
             $output .= '<span class="sub-menu-trigger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg></span>';
         }
     }
